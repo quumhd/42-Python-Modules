8c2c026ce3a6fef0392fbf34a6695fc913930d8b
Jens D <jdreissi@3-G-2.42heilbronn.de>
2026-03-24 15:12:11 +0100

    fixed flake8

--- Python-06/alchemy/__init__.py
+++ Python-06/alchemy/__init__.py
@@ -4,3 +4,5 @@ __author__ = "Master Pythonicus"
 
 
 from .elements import create_fire, create_water
+
+__all__ = ["create_fire", "create_water"]
--- Python-06/ft_circulat_curse.py
+++ Python-06/ft_circulat_curse.py
@@ -6,16 +6,22 @@ from alchemy.grimoire import record_spell, validate_ingredients
 def ft_circulat_curse() -> None:
     """wda"""
     print("=== Circular Curse Breaking ===\n")
-    print("Testing ingredients(\"fire air\"):", validate_ingredients("fire air"))
-    print("Testing ingredients(\"dragon scales\"):", validate_ingredients("dragon scales"))
+    print("Testing ingredient validation:")
+    print("Testing ingredients(\"fire air\"):",
+          validate_ingredients("fire air"))
+    print("Testing ingredients(\"dragon scales\"):",
+          validate_ingredients("dragon scales"))
     print()
-    print("Testing spell recording with validation")
-    print("record_spell(\"Fireball\"), (\"fire air\"):", record_spell("Fireball", "fire air"))
-    print("record_spell(\"Dark Magic\"), (\"shadow\"):", record_spell("Dark Magic", "shadow"))
+    print("Testing spell1 recording with validation")
+    print("record_spell(\"Fireball\"), (\"fire air\"):",
+          record_spell("Fireball", "fire air"))
+    print("record_spell(\"Dark Magic\"), (\"shadow\"):",
+          record_spell("Dark Magic", "shadow"))
     print()
     from alchemy.grimoire import record_spell as late_record_spell
     print("Testing late import technique")
-    print("late_record_spell(\"Lightning\"), (\"air\"):", late_record_spell("Lightning", "air"))
+    print("late_record_spell(\"Lightning\"), (\"air\"):",
+          late_record_spell("Lightning", "air"))
     print()
     print("Circular dependency curse avoided using late imports!")
     print("All spells processed safely!")
